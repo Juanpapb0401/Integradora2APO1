c73b78cbeb0e646ac18ec62e1c87723287d82804
Juan Parra <e5jparra@gmail.com>
2023-04-04 13:18:06 -0500

    Ahora el doc

---
+++ doc/API/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"model","l":"Capsula"},{"p":"model","l":"Controladora"},{"p":"model","l":"Etapa"},{"p":"ui","l":"GreenSOO"},{"p":"model","l":"Proyecto"},{"p":"model","l":"Capsula.Type"},{"p":"model","l":"Etapa.Type"}];updateSearchResults();
